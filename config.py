5bb4c97d2f383ffdc89bcf162d3bae90078edaa8
salmabenbrahim1 <benbrahimsalma22@gmail.com>
2024-12-20 23:38:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,2 @@
 NBA_2025_STANDINGS_URL = "https://www.basketball-reference.com/leagues/NBA_2025_standings.html"
 NBA_2025_PER_GAME_URL = "https://www.basketball-reference.com/leagues/NBA_2025_per_game.html"
-
-
-PER_GAME_OUTPUT_FILE = 'per_game_data.csv'
-EASTERN_CONFERENCE_OUTPUT_FILE = 'eastern_conference_data.csv'
-WESTERN_CONFERENCE_OUTPUT_FILE = 'western_conference_data.csv'
-
